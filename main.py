3cff97216ea11926b85182a15043f1b656b12a50
mayur savaliya <mayursavaliya150@gmail.com>
2023-06-02 11:31:04 +0530

    added progressBar

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pytube
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, jsonify
+from pytube.cli import on_progress
 from werkzeug.exceptions import abort
 import test
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "high"
 
+global video_reso
 
 @app.route('/')
 def Home():
@@ -16,7 +18,7 @@ def Home():
 def Videos():
     if request.method == 'POST':
         session['link'] = request.form['link']
-        yt = pytube.YouTube(session['link'])
+        yt = pytube.YouTube(session['link'], on_progress_callback=on_progress)
         img = yt.thumbnail_url
         title = yt.title
         return render_template('Videos.html', img=img, title=title)
@@ -27,7 +29,7 @@ def Videos():
 @app.route('/Download-144p')
 def Download_144():
     if 'link' in session:
-        yt = pytube.YouTube(session['link'])
+        yt = pytube.YouTube(session['link'],on_progress_callback=on_progress)
         yts = yt.streams.get_lowest_resolution()
         this = test.main(session['link'], video_reso=yts)
         return this
@@ -38,7 +40,7 @@ def Download_144():
 @app.route('/Download-360p')
 def Download_360():
     if 'link' in session:
-        yt = pytube.YouTube(session['link'])
+        yt = pytube.YouTube(session['link'],on_progress_callback=on_progress)
         yts = yt.streams.get_by_resolution('360p')
         this = test.main(session['link'], video_reso=yts)
         return this
@@ -49,7 +51,7 @@ def Download_360():
 @app.route('/Download-1080p')
 def Download_1080():
     if 'link' in session:
-        yt = pytube.YouTube(session['link'])
+        yt = pytube.YouTube(session['link'],on_progress_callback=on_progress)
         yts = yt.streams.get_highest_resolution()
         this = test.main(session['link'], video_reso=yts)
         return this
@@ -60,13 +62,21 @@ def Download_1080():
 @app.route('/Download-audio')
 def Download_audio():
     if 'link' in session:
-        yt = pytube.YouTube(session['link'])
+        yt = pytube.YouTube(session['link'],on_progress_callback=on_progress)
         yts = yt.streams.get_audio_only()
         this = test.main(session['link'], video_reso=yts)
         return this
     else:
         return '<h2> Enter Correct link </h2>'
 
+# @app.route('/progress')
+# def progress():
+#     # Your code to retrieve the progress percentage
+#     # percentage = test.on_progress(stream=video_reso, chunk=1024, bytes_remaining=video_reso.filesize) 
+#     #  # Replace with your logic to get the actual progress percentage
+#     test.p
+#     return jsonify({'progress': percentage})
+
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5555)
+    app.run(debug=True)
--- test.py
+++ test.py
@@ -8,13 +8,20 @@ def on_progress(stream, chunk, file_handle, bytes_remaining):
     percentage_of_completion = bytes_downloaded / total_size * 100
     return percentage_of_completion
 
+def progress_callback(stream, chunk, bytes_remaining):
+    total_bytes = stream.filesize
+    downloaded_bytes = total_bytes - bytes_remaining
+    percentage = int((downloaded_bytes / total_bytes) * 100)
+    return percentage
+
+global p
+
 
 def main(url, video_reso):
     try:
         chunk_size = 1024
         yt = YouTube(url)
         video = video_reso
-        yt.register_on_progress_callback(on_progress(video, chunk_size, 'YouTubeVidDownload', video.filesize))
         var0 = f"Fetching \"{video.title}\".."
         var = f"Fetching successful\n"
         var2 = (f"Information: \n"
@@ -22,9 +29,11 @@ def main(url, video_reso):
                 f"Resolution: {video.resolution}\n"
                 f"Author: {yt.author}")
         var3 = ("Views: {:,}\n".format(yt.views))
-        var4 = f"Downloading \"{video.title}\".."
+        var4 = f"Downloading \"{video.title} \".."
         video.download('YouTubeVidDownload')
         notify = 'Download success'
+        p = progress_callback(stream=video, chunk=1024, bytes_remaining=video.filesize)
+        print("p--------------", p)
         this = make_response(render_template('download.html', var0=var0, var=var, var2=var2, var3=var3, var4=var4, notify=notify))
         return this
     except Exception as e:
